ed117697a438459b5d3c433cfc4bf9da8e483c7e
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 11:03:19 -0500

    Create Code
    Build a .NET Standard Class Library and a Console Application. Put a couple calculation methods in it and call it from the Console.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
+using StandardLibrary;
+
 namespace ConsoleUI
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Calculator calculator = new Calculator();
+
+			int sum = calculator.Add(3, 4);
+			int difference = calculator.Subtract(4, 3);
+			int product = calculator.Multiply(3, 4);
+			int quotient = calculator.Divide(4, 3);
+			int modulus = calculator.Modulo(4, 3);
+
+			Console.WriteLine($"The sum of 3 and 4 is {sum}.");
+			Console.WriteLine($"The difference between 4 and 3 is {difference}.");
+			Console.WriteLine($"The product of 3 and 4 is {product}.");
+			Console.WriteLine($"The quotient of 4 and 3 is {quotient}.");
+			Console.WriteLine($"The modulus of 4 and 3 is {modulus}.");
 		}
 	}
 }
---
+++ StandardLibrary/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StandardLibrary
+{
+	public class Calculator
+	{
+		public int Add(int x, int y)
+		{
+			int output = x + y;
+			return output;
+		}
+
+		public int Subtract(int x, int y)
+		{
+			int output = x - y;
+			return output;
+		}
+
+		public int Multiply(int x, int y)
+		{
+			int output = x * y;
+			return output;
+		}
+
+		public int Divide(int x, int y)
+		{
+			int output = y != 0 ? x / y : throw new DivideByZeroException("You cannot divide by zero.");
+
+			return output;
+		}
+
+		public int Modulo(int x, int y)
+		{
+			int output = y != 0 ? x % y : throw new DivideByZeroException("You cannot divide by zero.");
+
+			return output;
+		}
+	}
+}
